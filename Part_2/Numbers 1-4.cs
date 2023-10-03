8927a054ea0707f985b48fd930c2b71ae10d6b4c
MaratByd04 <143888006+MaratByd04@users.noreply.github.com>
2023-10-04 01:25:14 +0300

    Подчистил лишнее.

--- Part_2/Numbers 1-4.cs
+++ Part_2/Numbers 1-4.cs
@@ -8,42 +8,10 @@ namespace Part_2
 {
     internal class Numbers
     {
-        static bool CalculateFactorial(int n, out long result)
-        {
-            result = 1;
+        
+           
 
-            try
-            {
-                checked
-                {
-                    for (int i = 2; i <= n; i++)
-                    {
-                        result *= i;
-                    }
-                }
-                return true; // Успешно вычислено
-            }
-            catch (OverflowException)
-            {
-                return false; // Произошло переполнение
-            }
-        }
-
-        static void Main()
-        {
-            int.TryParse(Console.ReadLine(), out int number); // Здесь можно указать желаемое число для вычисления факториала
-            long factorialResult;
-
-            if (CalculateFactorial(number, out factorialResult))
-            {
-                Console.WriteLine($"Факториал числа {number} равен {factorialResult}");
-            }
-            else
-            {
-                Console.WriteLine($"Ошибка: переполнение при вычислении факториала числа {number}");
-            }
-        }
-
+       
         
     }
 }
