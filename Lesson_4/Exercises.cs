488499df4048590d66f98bca90dc84474d02f0d2
MaratByd04 <143888006+MaratByd04@users.noreply.github.com>
2023-10-04 01:23:14 +0300

    Добавил упр 5.3

--- Lesson_4/Exercises.cs
+++ Lesson_4/Exercises.cs
@@ -9,7 +9,7 @@ namespace Part_1
     internal class Exercises
     {
             
-            static int LargerOfTwoNumbers(int firstNumber, int secondNumber)
+            static int LargerOfTwoNumbers(int firstNumber, int secondNumber) // Метод для упр 5.1.
             {
                 if (firstNumber > secondNumber)
                 {
@@ -24,7 +24,12 @@ namespace Part_1
 
 
             }
-            
+
+              static void ChangesOfVariable(ref int firstVariable, ref int secondVariable) // Метод для Упр 5.2
+              {
+                   (firstVariable, secondVariable) = (secondVariable, firstVariable);
+              }
+
         static void Main(string[] args)
         {
                 Console.WriteLine("Проверка Упражнения 5.1. Наибольшее из двух целых чисел.\n");
@@ -35,27 +40,66 @@ namespace Part_1
                 Console.WriteLine("Введите второе число.");
                 int.TryParse(Console.ReadLine(), out int secondNumber);
                 LargerOfTwoNumbers(firstNumber, secondNumber);
+                Console.ReadKey();
+                Console.Clear();
 
 
-                Console.WriteLine("\n\nУпражнение 5.2. Смена двух чисел.\n");
+                Console.WriteLine("Упражнение 5.2. Смена двух чисел.\n");
 
 
                 Console.WriteLine("Введите первое число.");
-                int firstVariable = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out var firstVariable);
                 Console.WriteLine("Введиет второе число.");
-                int secondVariable = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out var secondVariable);
                 Console.WriteLine("Первое число до смены " + firstVariable);
                 Console.WriteLine("Второе число до смены " + secondVariable);
                 ChangesOfVariable(ref firstVariable, ref secondVariable);
-                Console.WriteLine("Первое число после смены " + firstVariable);
+                Console.WriteLine("\nПервое число после смены " + firstVariable);
                 Console.WriteLine("Второе число после смены " + secondVariable);
+                Console.ReadKey();
+                Console.Clear();
 
+
+            Console.WriteLine("Упражнение 5.3. Факториал числа.\n");
+
+            Console.WriteLine("Введите число, факториал которого хотите получить.");
+            int.TryParse(Console.ReadLine(), out int numbForFactorial);
+            long resultOfFactorial;
+            if (Factorial(numbForFactorial, out resultOfFactorial))
+            {
+                Console.WriteLine("Факториал полученного числа равен = " + resultOfFactorial);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Переполнение!");
+            }
+            Console.ReadKey();
+            Console.Clear();
         }
 
-        static void ChangesOfVariable(ref int firstVariable, ref int secondVariable)
+       
+        static bool Factorial(int numbForFactorial, out long resultOfFactorial) // Метод для задания Упр 5.3
         {
-            (firstVariable, secondVariable) = (secondVariable, firstVariable);
+            resultOfFactorial = 1;
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i <= numbForFactorial; i++)
+                    {
+                        resultOfFactorial *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+
         }
+
+
 
     }
 }
