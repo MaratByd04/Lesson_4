b12f0d660c8d35593a5b6cdc924a28c383553a9a
MaratByd04 <143888006+MaratByd04@users.noreply.github.com>
2023-10-05 23:20:01 +0300

    .

--- Part_2/Numbers 1-4.cs
+++ Part_2/Numbers 1-4.cs
@@ -11,13 +11,46 @@ namespace Part_2
       
         public static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Black;
-            string s = Console.ReadLine();
-            Console.WriteLine(s.ToUpper());
+            Console.WriteLine("Номер 1. Смена элементов массива\n");
+
+
+            Random rnd = new Random();
+            int[] arrayForRandomNumbers = new int[20];
+            for (int i = 0; i < arrayForRandomNumbers.Length; i++)
+            {
+                arrayForRandomNumbers[i] = rnd.Next(1, 101);
+            }
+            Console.WriteLine("Изначальный массив равен:");
+            foreach (int i in arrayForRandomNumbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\nВведите первое чсило.");
+            int.TryParse(Console.ReadLine(), out int firstNumberFromArray);
+            Console.WriteLine("Введите второе число.");
+            int.TryParse(Console.ReadLine(), out int secondNumberFromArray);
+
+            if (firstNumberFromArray >= 0 && secondNumberFromArray >= 0 && firstNumberFromArray <= 19 && secondNumberFromArray <= 19)
+            {
+                Console.WriteLine("\nНовый массив равен:");
+                (arrayForRandomNumbers[firstNumberFromArray], arrayForRandomNumbers[secondNumberFromArray]) = (arrayForRandomNumbers[secondNumberFromArray], arrayForRandomNumbers[firstNumberFromArray]);
+                foreach (int i in arrayForRandomNumbers)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.Clear() ;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Вы ввели несуществующий индекс массива.");
+                Console.ResetColor();
+                Console.ReadKey();
+                Console.Clear();
+            }
+
         }
-
-
     }
 }
